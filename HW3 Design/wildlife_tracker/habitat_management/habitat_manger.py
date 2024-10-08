05dee1fddee7fe51d0932584b098d2260fffa099
Eugen Nesbakken <eugnes03@bu.edu>
2024-10-08 11:25:14 -0400

    PDF, screenshot uploaded, modules are done

--- HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
+++ HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
@@ -1,5 +1,36 @@
-from typing import Optional, List
+from typing import Optional, List, Any
+from wildlife_tracker.animal_management.animal import Animal
+from wildlife_tracker.habitat_management.habitat import Habitat
+
 
 class HabitatManager:
+    #Section 2: Habitat Management
+    def __init__(self) -> None:
+        habitats: dict[int, Habitat] = {}
+    #Done in order as the requirements
+    def create_habitat(habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
+        pass
+    def remove_habitat(habitat_id: int) -> None:
+        pass
 
-    pass
+    def get_habitats_by_size(size: int) -> List[Habitat]:
+        pass
+    def get_habitats_by_geographic_area(geographic_area: str) -> List[Habitat]:
+        pass
+    def get_habitat_by_id(habitat_id: int) -> Habitat:
+        pass
+    def get_habitats_by_type(environment_type: str) -> List[Habitat]:
+        pass
+
+    def get_habitat_details(habitat_id: int) -> dict:
+        pass
+
+    def get_animals_in_habitat(habitat_id: int) -> List[Animal]:
+        pass
+    def assign_animals_to_habitat(habitat_id: int, animals: List[Animal]) -> None:
+        pass
+    def update_habitat_details(habitat_id: int, **kwargs: dict[str, Any]) -> None:
+        pass
+    def assign_animals_to_habitat(animals: List[Animal]) -> None:
+        pass
+
--- HW3 Design/wildlife_tracker/migration_tracking/migration.py
+++ HW3 Design/wildlife_tracker/migration_tracking/migration.py
@@ -1,5 +1,23 @@
-from typing import Any
+from typing import Any, Optional
+from wildlife_tracker.habitat_management.habitat import Habitat
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath
+
 
 class Migration:
 
-    pass
+    def __init__(self, migration_id: int, destination: Habitat, migration_path: MigrationPath, start_date: str, start_location: Habitat, status: str = "Scheduled", duration: Optional[int] = None):
+        self.migration_id = migration_id
+        self.status = status
+        self.duration = duration
+        self.start_location = start_location
+        self.destination = destination
+        self.migration_path = migration_path
+        self.start_date = start_date
+        self.start_location = start_location
+    #Section 3: Migration Tracking
+    def update_migration_details(migration_id: int, **kwargs: Any) -> None:
+        pass
+    def get_migration_details(self)->dict[str,Any]:
+        pass
+    def cancel_migration(self) -> None:
+        pass
